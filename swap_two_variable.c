392af43ad83a45429b1b42d209c66fede282ebe3
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2022-03-02 14:48:32 +0530

    Create swap_two_variable.c

---
+++ swap_two_variable.c
@@ -0,0 +1,8 @@
+a=int(input("Enter first variable :"))
+b=int(input("Enter second variable :"))
+temp=a
+a=b
+b=temp
+print("variable after swap :")
+print("First variable :",a)
+print("Second variable :",b)
