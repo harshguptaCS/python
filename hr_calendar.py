9c24388160ea5d3df5b0df3bff3471d78698256b
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2022-03-21 13:05:14 +0530

    Create hr_calendar.py

---
+++ hr_calendar.py
@@ -0,0 +1,10 @@
+import calendar
+m,d,y=(int(i) for i in  input().split())
+x=calendar.weekday(y,m,d)
+if(x==0): print("MONDAY")
+elif(x==1): print("TUESDAY")
+elif(x==2): print("WEDNESDAY")
+elif(x==3): print("THURSDAY")
+elif(x==4): print("FRIDAY")
+elif(x==5): print("SATURDAY")
+else: print("SUNDAY")
